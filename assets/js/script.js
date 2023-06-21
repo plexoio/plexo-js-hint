e862aea702132271f5b2ad93f58c62ffc8aff595
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-21 11:12:47 +0000

    almost finish JSHint API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 
 // API interaction STARTS
 
-const API_KEY = 'y4FP_wupzW0R8uOFkBa6FtaF2mA'; // set it up
+const API_KEY = 'y4FP_wupzW0R8uOFkBa6FtaF2mAs'; // set it up
 const API_URL = 'https://ci-jshint.herokuapp.com/api'; // set it up
 const results_modal = new bootstrap.Modal(document.getElementById('resultsModal')); // bootstrap's method
 
@@ -47,11 +47,28 @@ const displayModal = (data) => {
 // POST data & process it
 
 /**
+ * Resolve issue of options: options - a comma separated list of options, which are outlined below. 
+ */
+
+function commaOptions(form) {
+    let myOptions = [];
+    for (let option of form.entries()) {
+        if (option[0] === 'options') {
+            myOptions.push(option[1]);
+        }
+    }
+    form.delete('options'); // delete occurrences of 'options'
+    form.append('options', myOptions.join()); // join() to convert back into a string 'commas' by default
+
+    return form;
+}
+
+/**
  * FIRST: POST function, sending/receiving data to/from the API
  */
 
 async function postForm(e) {
-    const form = new FormData(document.getElementById('checksform')); // capture all field & return an object
+    const form = commaOptions(new FormData(document.getElementById('checksform'))); // capture all field & return an object
 
     const initiatePost = await fetch(API_URL, { // two parameters for posting, 'destination & code block'
         method: "POST",
@@ -62,13 +79,34 @@ async function postForm(e) {
     });
     const returnedData = await initiatePost.json(); // parse JSON data to a JS object
 
-    if(initiatePost.ok){
+    if (initiatePost.ok) {
         displayData(returnedData);
-    }else {
-        throw new Error(returnedData.error);
+    } else {
+        displayPostError(returnedData);
+
+        let errorInfo = `${returnedData.error}\nStatus: ${returnedData.status_code}`;
+        errorInfo += `\nNumber: ${returnedData.error_no}`;
+        let myErrors = new Error(`${errorInfo}`);
+        console.log(myErrors);
     }
 }
 
+/**
+ * Display errors after trying to POST & receive response
+ */
+
+function displayPostError(whatErrors) {
+
+    const heading = 'JSHint Failed';
+    results = `<div><li>Error: <strong>${whatErrors.error}</strong></li></div>`
+    results += `<div><li>Status: <strong>${whatErrors.status_code}</strong></li></div>`
+    results += `<div><li>Number: <strong>${whatErrors.error_no}</strong></li></div>`
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    results_modal.show();
+}
 
 /**
  * SECOND: (POST): Function to display the data from the API manipulating the DOM
@@ -76,18 +114,18 @@ async function postForm(e) {
 
 const displayData = (data) => {
     let heading = `JSHint Results on: ${data.file}`;
-    if(data.total_errors === 0){
+    if (data.total_errors === 0) {
         results = `<div class="no_errors">All clean!</div>`;
-    }else {
+    } else {
         results = `<div>Errors Found: <span class="error_counts">${data.total_errors}</span></div>`;
 
-        for (let error of data.error_list){
+        for (let error of data.error_list) {
             results += `<div> At line: <span class="line">${error.line}</span>, `
             results += `column: <span>${error.col}</span></div>`;
             results += `<div>Error message: <span class="error-message">${error.error}</span></div>`;
         }
     }
-    
+
     document.getElementById('resultsModalTitle').innerText = heading;
     document.getElementById('results-content').innerHTML = results; // innerHTML
 
@@ -113,8 +151,8 @@ const displayData = (data) => {
 });
  */
 
-/**
- *     // for (entry of form.entries()){
+/** Testing code
+ *   // for (let entry of form.entries()){
     //     console.log(entry);
     // }
  */
