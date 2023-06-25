d87638d02ae8b80118fde6badc8026b9cb08cf90
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-25 10:41:34 +0000

    First Jest proper testing

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,5 +1,11 @@
-function addition () {
-    return 42
+/** 
+ * This addition function will receive string, integers and floats. 
+ * It will return a float number regardless of the datatype introduced as described above.
+ */
+function addition(num1, num2) {
+    let myNumbers = parseFloat(num1) + parseFloat(num2); // for string values
+    let floatNumber = myNumbers.toFixed(1); // result to string
+    return parseFloat(floatNumber); // result back to float fixed 1 decimal
 };
 
-module.export = addition;
+module.exports = addition;
--- assets/js/tests/calc.test.js
+++ assets/js/tests/calc.test.js
@@ -1,18 +1,31 @@
 const addition = require("../calc");
 
 describe('Calculator', () => {
+
     describe('Addition', () => {
-        test('Return 42 of 20 + 22', () => {
-            expect(addition(20,22)).toBe(42); // the function has to be defined
+        test('Return 42 if 20 + 22', () => { // set test
+            expect(addition(20, 22)).toBe(42.0); // set expectation, the function has to be defined
+        });
+
+        test('Return 73 if 41 + 32', () => { // set test
+            expect(addition('41', '32')).toBe(73.0); // passing Strings
+        });
+
+        test('Return 73.3 if 41.1 + 32.2', () => { // set test
+            expect(addition(41.1, 32.2)).toBe(73.3); // passing Floats
         });
     });
+
     describe('Substraction', () => {
         test('');
     });
+
     describe('Multiplication', () => {
         test('');
     });
+
     describe('Division', () => {
         test('');
     });
+
 });
